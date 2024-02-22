07b0b88093a244bc99acbe6482e59482fa9b04cc
criverromss <contact@criver.me>
2024-02-22 22:06:44 +0100

    first

--- js/script.js
+++ js/script.js
@@ -26,3 +26,40 @@ document.onkeydown = (e) => {
     e.preventDefault();
   }
 };
+
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  window.addEventListener('scroll', () => {
+    let scrollPosition = window.scrollY || document.documentElement.scrollTop;
+    let projectSection = document.querySelector('.project_section');
+    // Calculer le point où la barre de navigation doit apparaître
+    let triggerPoint = projectSection.offsetTop - window.innerHeight + 450;
+
+    let header = document.querySelector('header');
+
+    // Ajustez cette partie pour retarder l'apparition de la barre de navigation
+    if (scrollPosition > triggerPoint) {
+      header.style.display = 'block';
+    } else {
+      header.style.display = 'none';
+    }
+  });
+});
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  // Sélectionner l'élément par son ID
+  const downArrow = document.getElementById('down-arrow');
+
+  // Ajouter un écouteur d'événements pour le clic
+  downArrow.addEventListener('click', (e) => {
+    // Empêcher le comportement par défaut du navigateur
+    e.preventDefault();
+
+    // Sélectionner la section vers laquelle vous souhaitez défiler
+    const projectSection = document.querySelector('.project_section');
+
+    // Défiler vers la section project_section
+    projectSection.scrollIntoView({ behavior: 'smooth' });
+  });
+});
